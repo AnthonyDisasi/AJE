ba710fbf0ac02f3170a5ef3b7a0a0d543d8c8d10
AnthonyDisasi <anthonydisasi@gmail.com>
2020-10-15 20:11:51 +0200

    Travail sur l'action "Create" du controlleur "Eleve"

---
+++ AJE/Areas/Prefet/Controllers/ElevesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AJE.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AJE.Areas.Prefet.Controllers
+{
+    [Area("Prefet")]
+    public class ElevesController : Controller
+    {
+        private readonly AppDbContextEcole db;
+        public ElevesController(AppDbContextEcole _db)
+        {
+            db = _db;
+        }
+
+        public IActionResult Create(int )
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
